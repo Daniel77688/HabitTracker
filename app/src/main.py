4ddaf3a304768e3746579896877e850a8021c352
Daniel77688 <daniel77688@gmail.com>
2026-08-10 14:09:40 +0200

    main.py linked with controllers

---
+++ app/src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from src.models.database import init_db
+from src.controllers import habit_controller, habit_log_controller, users_controller, streak_controller
+
+init_db()
+
+app = FastAPI(title="Habit Tracker API", version="1.0.0",description="API REST for managing habits and streaks")
+
+app.include_router(habit_controller.router)
+app.include_router(habit_log_controller.router)
+app.include_router(users_controller.router)
+app.include_router(streak_controller.router)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the Habit Tracker. Go to /docs to view the interactive documentation."}
